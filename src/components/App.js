54ae86320a4666fdfb9d41ec7627beb5d412ac1e
Blake Creasser <blakecreasser@aggienetwork.com>
2022-12-17 16:14:43 -0600

    commit tree bc2d83b45f5bfce302fc1fbed7d1bcfddf4cfb94

--- src/components/App.js
+++ src/components/App.js
@@ -18,13 +18,32 @@ function Home() {
 
 {/* write an <About> component here */}
 
+function About() {
+  return (
+    <div id ='about'>
+      <h2>About</h2>
+    </div>
+  )
+}
+
+function Message() {
+  return (
+    <div>
+      <h1>This is just a test so i can see how react is working</h1>
+      <div>This is also in this section</div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
+      <Message />
     </div>
   );
 }
